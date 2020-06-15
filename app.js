d13d1a40a8ac30a68fc9e1cdbc53851926a934e9
Alex <>
2020-06-15 18:04:26 -0400

    test1
    test i guess

---
+++ app.js
@@ -0,0 +1,9 @@
+const joinGame_div = document.getElementById("join-game");
+
+joinGame_div.addEventListener('click', function() {
+
+})
+
+/*
+<button onclick="document.location = 'default.asp'">HTML Tutorial</button>
+*/
